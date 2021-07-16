84fcdbc71ffdc34d81bf8c1ec98335d15d3aa6ea
Николай Полтавский <n.poltavskiy@5-tv.ru>
2021-07-17 01:20:26 +0300

    start fenwick

--- 0_hero/FenwickBT.h
+++ 0_hero/FenwickBT.h
@@ -1,1 +1,26 @@
 #pragma once
+
+#include <vector>
+#include <type_traits>
+
+template<
+	typename NumericType,
+	typename = typename std::enable_if<std::is_arithmetic<NumericType>::value, NumericType>::type
+>
+class IndexedTree {
+public:
+	explicit IndexedTree() noexcept = default;
+	~IndexedTree(){}
+
+	const size_t Size() const noexcept {
+		return this->elems_.size();
+	}
+
+	const bool IsEmpty() const noexcept {
+		return this->elems_.empty();
+	}
+
+
+private:
+	std::vector<NumericType> elems_;
+};
